f2d3efaec43609a43229474fc74846e6b4cc1bec
Gabi08-I <senaioloko@gmail.com>
2026-02-19 11:19:14 -0300

    feat: implementar estrutura principal da aplicação e componente footer com estilos

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,11 +1,25 @@
-import "./App.css"
+import "./App.css";
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+import Rodape from "./components/Rodape/Rodape";
 
 const App = () => {
   return (
-    <div>
-      APP
-    </div>
-  )
-}
+    <div id="App">
+      <img
+        id="Logo"
+        src={logo}
+        alt="Logo do serviço de streaming DEVFLIX exibido em cores vibrantes, ideal para fãs de filmes e séries de TV"
+      />
 
-export default App
+      <div className="search">
+        <input type="text" placeholder="Pesquise por filmes e séries..." />
+        <img src={lupa} alt="Botão para a pesquisa" />
+      </div>
+
+      <Rodape>Hbi17.7</Rodape>
+    </div>
+  );
+};
+
+export default App;
