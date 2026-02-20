95e03dabc616009a3ea02ba58d2def0311c3212e
Gabi08-I <senaioloko@gmail.com>
2026-02-20 11:00:05 -0300

    feat: atualizar configuração da API OMDB e adicionar estilos ao componente MovieCard

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,53 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+
 import logo from "./assets/devflix.png";
 import lupa from "./assets/search.svg";
+
 import Rodape from "./components/Rodape/Rodape";
-import { useEffect } from "react";
 import MovieCard from "./components/MovieCard/MovieCard";
 
 const App = () => {
-
   const [movies, setMovies] = useState([]);
 
-  //UTILIZANDO CHAVE DE API DO ARQUIVO .ENV
+  //Utilizando uma CHAVE de API do arquivo .env
   const apiKey = import.meta.env.VITE_OMDB_API_KEY;
-  const apiUrl = `httpa://omdbapi.com/?apikey=${apiKey}`;
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
 
-  //CRIANDO A CONEXÃO COM A API E TRAZENDO INFORMAÇÕES
+  //Criando a conexão com a API e trazendo informações
   const searchMovies = async (title) => {
-    const response = await fetch (`$(apiUrl)&s=$(title)`);
-    const data = await response.json;
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json();
 
-    //ALIMENTANDO A VAR MOVIES 
+    //Alimentando a variavel movies
     setMovies(data.Search);
   };
 
   useEffect(() => {
     searchMovies("Batman");
-  }, [])
+  }, []);
 
   return (
     <div id="App">
       <img
         id="Logo"
         src={logo}
-        alt="Logo do serviço de streaming DEVFLIX exibido em cores vibrantes, ideal para fãs de filmes e séries de TV"
+        alt="Logotipo do serviço de streaming Devflix, com letras vermelhas e fundo preto, promovendo conteúdo de séries, filmes e entretenimento online."
       />
 
       <div className="search">
-        <input type="text" placeholder="Pesquise por filmes e séries..." />
-        <img src={lupa} alt="Botão para a pesquisa" />
+        <input type="text" placeholder="Pesquise por filmes" />
+        <img src={lupa} alt="Botão de ação para pesquisa!" />
       </div>
 
-    <div className="container">
-      {movies.map((mpvie, index) =>
-       <MovieCard />
-      )}
-    </div>
+{movies?.length > 0 ? (
+
+<div className="container">
+        {movies.map((movie, index) => (
+          <MovieCard key={index} {...movie} />
+        ))}
+      </div>
+):
+
+(<h2>Filme não encontrado :,( </h2>)
+}
 
 
-       <Rodape link={"https://github.com/Gabi08-l"}>Hbi17.7</Rodape>
+    
+
+      <Rodape link={"https://github.com/ProfCastello"}>ProfCastello</Rodape>
     </div>
   );
 };
