0ed614ac890121c8454cf06bc64d343c8dbd5562
Gabi08-I <senaioloko@gmail.com>
2026-03-12 16:54:55 -0300

    Funcionalidade: Inicialização de um projeto React Router com estrutura e componentes básicos
    - Adicionado o arquivo package.json com as dependências do React, React Router e Vite.
    
    - Criada a estrutura principal da aplicação, incluindo componentes do aplicativo, rotas e páginas (Home, Sobre, Contato).
    
    - Implementados os componentes Header e Rodape para navegação e rodapé.
    
    - Adicionados estilos CSS para layout e design.
    
    - Incluído um formulário na página Contato para entrada de dados do usuário.
    
    - Configuração do Vite para o servidor de desenvolvimento.
    
    - Adicionados recursos SVG para a identidade visual da marca.
    
    - Implementada a página 404 Not Found para rotas não encontradas.

---
+++ 05-react-router-top/src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Sobre from "./pages/Sobre";
+import Home from "./pages/Home";
+import Header from "./components/header/Header";
+import Rodape from "./components/rodape/Rodape";
+import Contato from "./pages/Contato";
+import Gato from "./assets/OIP.webp"
+function App() {
+  return (
+    <>
+    <Header />
+      <Routes>
+        {" "}
+        {/*identifica todas as rotas do sistema*/}
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} /> {/*Uma rota do sistema*/}
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+
+
+
+
+
+        <Rodape link={"https://github.com/Gabi08-l"}>Hbi17.7</Rodape>
+    </>
+  );
+}
+
+export default App;
