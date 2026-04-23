32e1264f70cfa1a771f26a98496ab215bc6deca1
Gabi08-I <senaioloko@gmail.com>
2026-04-23 17:02:19 -0300

    Refatorar aplicação de chat com novos componentes e estilos
    Removidos ícones SVG e assets não utilizados.
    Atualizado componente App para gerenciar visibilidade do chat e conexão do socket.
    Aprimorado componente Join com Material-UI para melhor experiência do usuário.
    Adicionado componente Chat para gerenciar exibição de mensagens e entrada de dados.
    Criados novos módulos CSS para componentes Chat e Join com estilos melhorados.
    Atualizada configuração do Vite para alterar porta do servidor.
    Ajustadas configurações CORS do servidor para permitir todas as origens em desenvolvimento.

--- 06-devChat/client/src/App.jsx
+++ 06-devChat/client/src/App.jsx
@@ -1,16 +1,25 @@
 
 import './App.css'
 import Join from './Components/Join/Join'
-
+import Chat from './Components/Chat/Chat'
+import { useState } from 'react';
 
 function App() {
-  
+  const [chatVisibility, setChatVisibility] = useState(false);
+  const [socket, setSocket] = useState(null);
 
   return (
     <>
-     <Join />
+    {chatVisibility ? (
+
+          <Chat socket={socket} /> )
+:
+(
+ <Join setSocket={setSocket} setChatVisibility={setChatVisibility} />
+  )}
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
